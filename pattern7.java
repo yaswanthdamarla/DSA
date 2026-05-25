8d6f061c7b5b717733809b40452de0481821c70a
Yaswanth Damarla <yaswanthdamarla9@gmail.com>
2026-05-25 17:56:38 +0530

    adding save and changes

--- pattern7.java
+++ pattern7.java
@@ -16,10 +16,30 @@ public class pattern7 {
         public void geroge(int n){
             for(int i=0; i<n; i++)
             {
+                //* Left spaces
                 for(int j=0; j<n-i-1; j++)
                 {
                     System.out.print(" ");
-            }
+                }
+                //*The stars position
+                for(int j=0; j<2*i+1;j++)
+                {
+                    System.out.print("*");
+                }
+                //* Right spaces
+                for(int j=0 ;j<n-i-1;j++)
+                {
+                    System.out.print(" ");
+                }
+                System.out.println();
         }
     }
 }
+
+// Enter the size:6
+//      *     
+//     ***    
+//    *****   
+//   *******  
+//  ********* 
+// ***********
